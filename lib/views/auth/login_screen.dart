d7a7c53293c41d12499606e7bcc80d578822be7b
chanddwi780-cpu <chanddwi780@gmail.com>
2026-01-19 18:07:43 +0700

    fix: change login text to force cache bust

--- lib/views/auth/login_screen.dart
+++ lib/views/auth/login_screen.dart
@@ -162,7 +162,7 @@ class _LoginScreenState extends State<LoginScreen> {
                               ),
                             ),
                             child: const Text(
-                              'Login',
+                              'Masuk ke Akun',
                               style: TextStyle(
                                 fontSize: 16,
                                 fontWeight: FontWeight.bold,
